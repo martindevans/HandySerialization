6993615d70106d1151512d6ba6f25f31f393bdc0
Martin Evans <martindevans@gmail.com>
2024-06-05 20:55:26 +0100

    Added `MemoryWrapper`

---
+++ HandySerialization.Tests/MemoryWrapperTests.cs
@@ -0,0 +1,29 @@
+﻿using HandySerialization.Extensions;
+using HandySerialization.Wrappers;
+
+namespace HandySerialization.Tests;
+
+[TestClass]
+public class MemoryWrapperTests
+{
+    [TestMethod]
+    public void RoundTripMemory()
+    {
+        var bytes = new byte[128];
+
+        var writer = new MemoryByteWriter(bytes);
+        writer.WriteVariableInt64(1);
+        writer.WriteVariableInt64(22);
+        writer.WriteVariableInt64(333);
+        writer.WriteVariableInt64(4444);
+        writer.WriteVariableInt64(55555);
+
+        var reader = new MemoryByteReader(bytes.AsMemory(0, writer.Written));
+        Assert.AreEqual(1, reader.ReadVariableInt64());
+        Assert.AreEqual(22, reader.ReadVariableInt64());
+        Assert.AreEqual(333, reader.ReadVariableInt64());
+        Assert.AreEqual(4444, reader.ReadVariableInt64());
+        Assert.AreEqual(55555, reader.ReadVariableInt64());
+        Assert.AreEqual(0, reader.UnreadBytes);
+    }
+}
---
+++ HandySerialization/Wrappers/MemoryWrapper.cs
@@ -0,0 +1,26 @@
+﻿namespace HandySerialization.Wrappers;
+
+public struct MemoryByteWriter(Memory<byte> memory)
+    : IByteWriter
+{
+    public int Written { get; private set; }
+
+    public void Write(ReadOnlySpan<byte> value)
+    {
+        value.CopyTo(memory[Written..].Span);
+        Written += value.Length;
+    }
+}
+
+public struct MemoryByteReader(ReadOnlyMemory<byte> memory)
+    : IByteReader
+{
+    private int _readBytes = 0;
+    public readonly long UnreadBytes => memory.Length - _readBytes;
+
+    public void ReadBytes(Span<byte> dest)
+    {
+        memory.Slice(_readBytes, dest.Length).Span.CopyTo(dest);
+        _readBytes += dest.Length;
+    }
+}
